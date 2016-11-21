8947402c03cf729147acb4f6367b612c431cf9e4
Gelber <AGELBER@arinc.com>
2016-11-21 15:44:05 -0600

    initial dataset models

--- testlibrary/admin.py
+++ testlibrary/admin.py
@@ -12,7 +12,7 @@ class StepAdmin(admin.ModelAdmin):
 	fields = ['case', 'case_version', 'step_text', 'result_text', 'order']
 	
 class CaseAdmin(admin.ModelAdmin):
-    fields = ['creation_date', 'title', 'case_text', 'case_type']
+    fields = ['creation_date', 'title', 'case_text', 'case_type', 'data_sets']
     inlines = [StepInline]	
 
 class CaseTypeAdmin(admin.ModelAdmin):
@@ -21,9 +21,17 @@ class CaseTypeAdmin(admin.ModelAdmin):
 class HistoryAdmin(admin.ModelAdmin):
 	fields = ['revision_number', 'case']
 
+class DataSetAdmin(admin.ModelAdmin):
+	fields = ['title', 'data_fields']
+
+class DataFieldAdmin(admin.ModelAdmin):
+	fields = ['title', 'data_type']
+	
 admin.site.register(Case, CaseAdmin)
 admin.site.register(CaseType, CaseTypeAdmin)
 admin.site.register(Step, StepAdmin)
 admin.site.register(CaseHistory, HistoryAdmin)
+admin.site.register(DataSet, DataSetAdmin)
+admin.site.register(DataField, DataFieldAdmin)
 
 # Register your models here.
--- testlibrary/models.py
+++ testlibrary/models.py
@@ -11,12 +11,37 @@ class CaseType(BusinessObject):
     def __str__(self):
         return self.case_type
 
+	
+class DataField(BusinessObject):
+	title = models.CharField(max_length=200, default='New Data Field')
+	TYPE_CHOICES = (
+		('string','String'),
+		('number','Number'),
+		('boolean','Boolean')
+	)
+	data_type = models.CharField(max_length=30, choices=TYPE_CHOICES, default='string')
+	
+	def __str__(self):
+		return self.title
+	
+class DataSet(BusinessObject):
+	title = models.CharField(max_length=200, default='New Data Set')
+	data_fields = models.ManyToManyField(DataField)
+	
+	def __str__(self):
+		return self.title
+	
+	
 class Case(BusinessObject):
-    case_text = models.CharField(max_length=200)
-    title = models.CharField(max_length=200, default='TestCase')
-    creation_date = models.DateTimeField('date published')
-    version = models.IntegerField(default=1)
-    case_type = models.ForeignKey('CaseType')
+	case_text = models.CharField(max_length=200)
+	title = models.CharField(max_length=200, default='TestCase')
+	creation_date = models.DateTimeField('date published')
+	version = models.IntegerField(default=1)
+	case_type = models.ForeignKey('CaseType', null=True)
+	data_sets = models.ManyToManyField(DataSet)
+	
+	def __str__(self):
+		return self.title
 
 class CaseHistory(BusinessObject):
 	revision_number = models.IntegerField(default=1)
@@ -51,4 +76,3 @@ class Release(BusinessObject):
 	)
 	
 	status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='scheduled')
-	
